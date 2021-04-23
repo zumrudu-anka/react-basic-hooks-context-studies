ca7981e59002b16f1ee01caaf890778e531cd404
Zümrüd-ü Anka <osmandurdag@hotmail.com>
2021-04-23 23:40:37 +0300

    added routing

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./assets/css/styles.css";
+import { BrowserRouter } from 'react-router-dom';
+
+const appName = "/react-basic-hooks-context-studies";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter basename = {appName}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
